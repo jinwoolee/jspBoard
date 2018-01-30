e046187e9ba468777635d9b26290c904cf755f95
jw <jwlee1728@gmail.com>
2018-01-30 10:02:30 +0900

    bin 삭제

--- bin/WebContent/WEB-INF/db/1.db스키마.sql
+++
@@ -1,2 +0,0 @@
-create user jspboardTest identified by jspboardtest default tablespace pc01 temporary tablespace temp quota unlimited on pc01 quota 0m on system;
-grant resource, connect to jspboardTest;
--- bin/WebContent/WEB-INF/db/1.db스키마_mac.sql
+++
@@ -1,2 +0,0 @@
-create user jspboardTest identified by jspboardTest default tablespace users temporary tablespace temp quota unlimited on users quota 0m on system;
-grant resource, connect to jspboardTest;
